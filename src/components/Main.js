ff0ab495a40cb00679290b3fb3ee59dc3e60d24e
Mark Abuyabor <mark@zeguestlist.com>
2022-09-17 20:33:16 +0800

    adding the click active

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
-import $ from "jquery";
+import $, { Callbacks } from "jquery";
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             beats: [
-                {id: 'Q', code: 113, url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', name: "Heater 1"},
-                {id: 'W', code: 119, url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', name: "Heater 2" },
-                {id: 'E', code: 101, url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', name: "Heater 3"},
-                {id: 'A', code: 97, url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', name: "Heater 4"},
-                {id: 'S', code: 115, url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', name: "Clap"},
-                {id: 'D', code: 100, url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', name: "Open-HH" },
-                {id: 'Z', code: 122, url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', name: "Kick-n'-Hat"},
-                {id: 'X', code: 120, url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', name: "Kick"},
-                {id: 'C', code: 99, url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', name: "Closed-HH"}
+                {id: 'Q',  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', name: "Heater 1"},
+                {id: 'W',  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', name: "Heater 2" },
+                {id: 'E', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', name: "Heater 3"},
+                {id: 'A', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', name: "Heater 4"},
+                {id: 'S', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', name: "Clap"},
+                {id: 'D', url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', name: "Open-HH" },
+                {id: 'Z', url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', name: "Kick-n'-Hat"},
+                {id: 'X', url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', name: "Kick"},
+                {id: 'C', url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', name: "Closed-HH"}
             ],
             display: ""
         }
@@ -59,6 +59,13 @@ class Main extends Component {
             }
 
             $("#" + charAddedClass + "-pad").addClass("active").siblings().removeClass("active");
+            
+        });
+
+        $(".drum-pad").click(function(e) {
+            let clickedKey = e.target.innerText;
+
+            $("#" + clickedKey + "-pad").addClass("active").siblings().removeClass("active");
 
         });
     }
